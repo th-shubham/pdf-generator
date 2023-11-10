77117c3008666ae340672e84e66d50eaeda59579
Shubham Jangid <shubham.jangid@astonous.com>
2023-11-10 20:36:46 +0530

    Refactor main method indentation in PdfGeneratorApplication.java.

--- src/main/java/com/example/pdfgenerator/PdfGeneratorApplication.java
+++ src/main/java/com/example/pdfgenerator/PdfGeneratorApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PdfGeneratorApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PdfGeneratorApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PdfGeneratorApplication.class, args);
+    }
 }
